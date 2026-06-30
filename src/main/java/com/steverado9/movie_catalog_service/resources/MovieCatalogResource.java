ec4dcfd849eebf20cbf5262aabaf82586d0d826e
stephen <isaacsteverado9@gmail.com>
2026-06-30 01:48:58 +0100

    created movie catalog service

---
+++ src/main/java/com/steverado9/movie_catalog_service/resources/MovieCatalogResource.java
@@ -0,0 +1,9 @@
+package com.steverado9.movie_catalog_service.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieCatalogResource {
+
+    public List<catalogItem> getCatalog(String userId) {}
+}
